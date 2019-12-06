28a7798b4d9e1815ae03e3dcea1cb22a1aaeabc5
平台中心/技术部-黄涛 <huangtao@aurogon.com>
2019-12-06 10:17:53 +0800

    some fixes

--- auth.go
+++ auth.go
@@ -36,13 +36,14 @@ func NewOpenId(r *http.Request) *OpenId {
 	id.root = proto + r.Host
 
 	uri := r.RequestURI
-	if i := strings.Index(uri, "openid"); i != -1 {
+	if i := strings.Index(uri, "openid."); i != -1 {
 		uri = uri[0 : i-1]
 	}
 	id.returnUrl = id.root + uri
 
 	switch r.Method {
 	case "POST":
+		r.ParseForm()
 		id.data = r.Form
 	case "GET":
 		id.data = r.URL.Query()
@@ -52,24 +53,15 @@ func NewOpenId(r *http.Request) *OpenId {
 }
 
 func (id OpenId) AuthUrl() string {
-	data := map[string]string{
-		"openid.claimed_id": openId_identifier,
-		"openid.identity":   openId_identifier,
-		"openid.mode":       openId_mode,
-		"openid.ns":         openId_ns,
-		"openid.realm":      id.root,
-		"openid.return_to":  id.returnUrl,
-	}
+	data := make(url.Values)
+	data.Set("openid.claimed_id", openId_identifier)
+	data.Set("openid.identity", openId_identifier)
+	data.Set("openid.mode", openId_mode)
+	data.Set("openid.ns", openId_ns)
+	data.Set("openid.realm", id.root)
+	data.Set("openid.return_to", id.returnUrl)
 
-	i := 0
-	url := steam_login + "?"
-	for key, value := range data {
-		url += key + "=" + value
-		if i != len(data)-1 {
-			url += "&"
-		}
-		i++
-	}
+	url := steam_login + "?" + data.Encode()
 	return url
 }
 
